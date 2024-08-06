195b258d88f6724a3cd3f97186064e85ef772b3a
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-08-06 16:59:11 +0530

    first commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+
+const Container = ( {children} ) => {
+  return (
+    <>
+      <div className="card" style={{width: "100%"}}>
+        <div className="card-body">
+          { children }
+        </div>
+      </div>
+    </>
+  )
+}
+export default Container;
